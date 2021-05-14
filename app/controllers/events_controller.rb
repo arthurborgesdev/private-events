a4dabb5696c1f54ed03b97180bcca5d75924f7c3
jmontanholi <jonunes9326@hotmail.com.br>
2021-05-14 14:20:56 -0300

    Finish events views

---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,63 @@
+class EventsController < ApplicationController
+
+  def index
+    @events = Event.all
+  end
+
+  def show
+    @event = Event.find(params[:id])
+  end
+
+  def new
+    @event = User.find(session[:current_user_id]).events.build
+  end
+
+  def edit
+  end
+
+  def create
+    @event = User.find(session[:current_user_id]).events.new(event_params)
+
+    respond_to do |format|
+      if @event.save
+
+        format.html { redirect_to @event, notice: "Event was successfully created." }
+        format.json { render :show, status: :created, location: @event }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @event.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @event.update(user_params)
+        format.html { redirect_to @event, notice: "Event was successfully updated." }
+        format.json { render :show, status: :ok, location: @event }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @event.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @event.destroy
+    respond_to do |format|
+      format.html { redirect_to events_url, notice: "Event was successfully destroyed." }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+  # Use callbacks to share common setup or constraints between actions.
+  def set_event
+    @event = Event.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def event_params
+    params.require(:event).permit(:name, :date, :description)
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_14_153544) do
+ActiveRecord::Schema.define(version: 2021_05_14_165920) do
 
   create_table "events", force: :cascade do |t|
     t.date "date"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2021_05_14_153544) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "author_id"
+    t.text "description"
   end
 
   create_table "users", force: :cascade do |t|
