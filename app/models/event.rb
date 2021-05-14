21e28343941b16ca60df3b0e246d4a2b99f211e4
Arthur Borges <arthurborges.dev@gmail.com>
2021-05-14 17:15:21 -0300

    Add feature to show events, future events and previous ones

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,7 @@ class Event < ApplicationRecord
   belongs_to :author, class_name: "User"
   has_many :attendance_lists, foreign_key: :scheduled_event_id
   has_many :attendants, through: :attendance_lists, source: :event_attendant
+
+  scope :past, -> { where('date < ?', Date.today) }
+  scope :upcoming, -> { where('date >= ?', Date.today) }
 end
